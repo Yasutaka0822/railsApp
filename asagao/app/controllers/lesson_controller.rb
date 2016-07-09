94ae21e03a14aa54e1c0356cfb9e15825dc4d608
Yasutaka0822 <y_shimizu@dreamarts.co.jp>
2016-07-09 10:15:22 +0900

    param test

--- asagao/app/controllers/lesson_controller.rb
+++ asagao/app/controllers/lesson_controller.rb
@@ -1,4 +1,13 @@
 class LessonController < ApplicationController
   def step1
     render text: "param test Mr.#{params[:name]}"
+  end
+
+  def step2
+    render text: params[:controller] + "#" +  params[:action]
+  end
+
+  def step22
+    render text: request.env["HTTP_USER_AGENT"] + request.headers["User-Agent"]
+  end
 end
