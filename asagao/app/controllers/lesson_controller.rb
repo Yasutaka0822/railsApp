1160a9ada1bca8e31e241f28199f7d1846ccba4c
Yasutaka0822 <y_shimizu@dreamarts.co.jp>
2016-07-09 10:49:22 +0900

    lesson4-7

--- asagao/app/controllers/lesson_controller.rb
+++ asagao/app/controllers/lesson_controller.rb
@@ -1,4 +1,7 @@
 class LessonController < ApplicationController
+  before_action :set_message, only: :step7
+
+
   def step1
     render text: "param test Mr.#{params[:name]}"
   end
@@ -12,10 +15,28 @@ class LessonController < ApplicationController
   end
 
   def step3
-    redirect_to action: "step4"
+    redirect_to action: "step4" , status: 301
   end
 
   def step4
     render text: "move to step4"
+  end
+
+  def step5
+    flash[:notice] = "move to step6"
+    redirect_to action: "step6"
+  end
+
+  def step6
+    render text: flash[:notice]
+  end
+
+  def step7
+    render text: @message
+  end
+
+  private
+  def set_message
+    @message = "hello"
   end
 end
